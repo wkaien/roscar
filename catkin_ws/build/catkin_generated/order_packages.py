6afad73370b1d7a5f77d5afa1e2687d9a17c41df
KurisaW <2053731441@qq.com>
2022-11-03 21:17:55 +0800

    [modify] 修改层次结构

--- catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/ks/tutorial_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/ks/ldlidar_ros_ws/devel;/home/ks/tutorial_ws/devel;/opt/ros/melodic'.split(';') if '/home/ks/ldlidar_ros_ws/devel;/home/ks/tutorial_ws/devel;/opt/ros/melodic' != '' else []
